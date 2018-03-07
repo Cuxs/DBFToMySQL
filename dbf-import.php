88ae04b134af4fb9174d0e20322e99f6fe9156db
Mariano Fliri <mfliri@as-one.com.ar>
2018-03-07 09:09:51 -0300

    Switch to postgres convertion

--- dbf-import.php
+++ dbf-import.php
@@ -2,10 +2,11 @@
 
 @include "config.php";			// please copy the config.sample.php and edit the correct fields
 
-@include "classes/XBase/Table.php";
-@include "classes/XBase/Column.php";
-@include "classes/XBase/Record.php";
-@include "classes/DBFhandler.php";
+@include "./php-xbase/src/XBase/Table.php";
+@include "./php-xbase/src/XBase/Column.php";
+@include "./php-xbase/src/XBase/Record.php";
+@include "./php-xbase/src/XBase/Memo.php";
+@include "./classes/DBFhandler.php";
 
 use XBase\Table;
 
@@ -16,12 +17,18 @@ set_time_limit( 0 );
 $time_start = time();
 $files = scandir($xbase_dir) or die ("Error! Could not open directory '$xbase_dir'.");
 $conn = new mysqli($db_host, $db_uname, $db_passwd, $db_name) or die ("Error connecting to mysql $mysqli->connect_error");
+mysqli_set_charset($conn, 'utf8');
+
+
 
 foreach ($files as $file) {
   switch ($file) {
   case (preg_match("/dbf$/i", $file) ? true : false):
-  	print_r("DBF: $file\n");
-  	dbftomysql($file);
+	  print_r("DBF: $file\n");	
+	  $tbl = substr($file,0,strlen($file)-4);
+	  echo ("$tbl.sql");
+      $sqlFile = fopen("convertedTables/$tbl.sql", "w") or die("Unable to open file!");	    
+  	dbftoPostgres($file, $sqlFile);
   	break;
   default:
   	print_r("Other file: $file\n");
@@ -31,7 +38,7 @@ foreach ($files as $file) {
 $time_end = time();
 "\n\nImport finished! Time spent: ". round( ( $time_end - $time_start ) / 60, 2 ) ." minutes\n";
 
-function dbftomysql($file) {
+function dbftoPostgres($file, $sqlFile) {
 	// Path to dbase file
 	global $xbase_dir;
 	global $conn;
@@ -41,58 +48,54 @@ function dbftomysql($file) {
 	// Open dbase file
 	$table = new Table($db_path);
 	$tbl = substr($file,0,strlen($file)-4);
+	print ( "Beware: space of C (Character field) - will be double in MySQL for the special case of a full special-chars-only field\n" );
 	print_r ("$tbl");
 	$line = array();
+	$line[]="id SERIAL";		
 
 	foreach ($table->getColumns() as $column) {
 		print_r("\t$column->name ($column->type / $column->length)\n");
 		switch($column->type) {
 			case 'C':	// Character field
-				$line[]= "`$column->name` VARCHAR($column->length)";
+				$line[]= "$column->name VARCHAR(" . $column->length * 2 . ")"; // double space for utf-8 conversion of special chars
 				break;
 			case 'F':	// Floating Point
-				$line[]= "`$column->name` FLOAT";
+				$line[]= "$column->name FLOAT";
 				break;
 			case 'N':	// Numeric
-				$line[]= "`$column->name` INT";
+				$line[]= "$column->name NUMERIC(" . $column->length . ")";
 				break;
 			case 'L':	// Logical - ? Y y N n T t F f (? when not initialized).
-				$line[]= "`$column->name` TINYINT";
+				$line[]= "$column->name TINYINT";
 				break;
 			case 'D':	// Date
-				$line[]= "`$column->name` DATE";
+				$line[]= "$column->name DATE";
 				break;
 			case 'T':	// DateTime
-				$line[]= "`$column->name` DATETIME";
+				$line[]= "$column->name DATETIME";
 				break;
 			case 'M':	// Memo type field
 			default:
-				$line[]= "`$column->name` TEXT";
+				$line[]= "$column->name TEXT";
 				break;
 		}
 	}
 
 	$str = implode(",",$line);
-	$sql = "CREATE TABLE IF NOT EXISTS `$tbl` ( $str );";
+	
+	$sql = 'BEGIN; SET statement_timeout=60000; DROP TABLE IF EXISTS '.$tbl.'; CREATE TABLE '.$tbl.'('.$str.'); ';
 
-	if ($conn->query("$sql") === TRUE) {
-		echo "Table $tbl successfully created\n";
-	} else {
-		echo "Error SQL: ".$conn->error ." >> $sql \n";
-		if ($die_on_mysql_error) {
-			die;
-		}
-	}
-
+	fwrite($sqlFile, $sql);
 	$table->close();
 
 	// Import using dbf + fpt files (for MEMO data...)
 	$fpt_file = str_replace( '.dbf', '.fpt', $db_path );
 	$fpt_path = ( file_exists( $fpt_file ) ? $fpt_file : '' );
-	import_dbf_to_mysql( $tbl, $db_path, $fpt_path );
+	import_dbf_to_postgres( $table, $tbl, $db_path, $fpt_path, $sqlFile );
 //	import_dbf($db_path, $tbl);
 }
 
+// Not used (see above)
 function import_dbf($db_path, $tbl) {
 	global $conn;
 	global $die_on_mysql_error;
@@ -117,7 +120,7 @@ function import_dbf($db_path, $tbl) {
 					$line[]=sprintf("%7.2f", $record->getObject($column) );
 					break;
 				case 'N':	// Numeric
-					$line[]=sprintf("%d", $record->getObject($column) );
+					$line[]=sprintf("'%s'", $record->getObject($column) );
 					break;
 				case 'L':	// Logical - ? Y y N n T t F f (? when not initialized).
 					// $line[] = sprintf("%d", ($record->getBoolean($column) ? 1 : 0) );
@@ -133,7 +136,7 @@ function import_dbf($db_path, $tbl) {
 		$val = implode(",",$line);
 		$col = implode(",",$fields);
 
-        if($GLOBALS['from_encoding']!="")$val = mb_convert_encoding($val, 'UTF-8', $GLOBALS['from_encoding'] );
+		if($GLOBALS['from_encoding']!="")$val = iconv($GLOBALS['from_encoding'], 'UTF-8', $val);
 
 		$sql = "INSERT INTO `$tbl` ($col) VALUES ($val)\n";
 		// print_r ("$sql");
@@ -155,49 +158,79 @@ function import_dbf($db_path, $tbl) {
 
 }
 
-function import_dbf_to_mysql( $table, $dbf_path, $fpt_path ) {
-	echo "Initializing import: Table $table\n";
+
+function import_dbf_to_postgres( $table, $tbl, $dbf_path, $fpt_path, $sqlFile ) {
+	echo "Initializing import: Table $tbl\n";
 	global $conn;
 	global $die_on_mysql_error;
-    $i = 0;
-    $Test = new DBFhandler( $dbf_path, $fpt_path );
+	$i = 0;
+	$Test = new DBFhandler( $dbf_path, $fpt_path );
+	$sql1 = "INSERT INTO $tbl (";
+	$sql2 = "values";
+    $sql = "";
+	$row = array();
+	foreach ($table->getColumns() as $column) {
+		$sql1 .="$column->name,";
+	}
+	$sql1=rtrim($sql1,", ");
+	$sql1.=")";
+
     while ( ($Record = $Test->GetNextRecord( true )) and ! empty( $Record ) ) {
-        $a = 0;
-        $sql1 = "INSERT INTO `$table` (";
-        $sql2 = ") values (";
-        $sql = "";
-        foreach ( $Record as $key => $val ) {
-        	$key = (strpos($key, 0x00) !== false ) ? substr($key, 0, strpos($key, 0x00)) : $key;
+		$a = 0;
+		$sql3="(";
+		$sql4="";
+        foreach ( $Record[ 'type' ] as $key => $type ) {
+            $val = $Record[ 'value' ][ $key ];
+            $key = (strpos($key, 0x00) !== false ) ? substr($key, 0, strpos($key, 0x00)) : $key;
 
             if ( $val == '{BINARY_PICTURE}' ) {
                 continue;
             }
+            switch( $type ) {
+	            case 'C':	// Character field
+	            case 'M':	// Memo type field
+		            $val = sprintf("'%s'", $val );
+		            break;
+	            case 'F':	// Floating Point
+		            $val = sprintf("%7.2f", $val );
+		            break;
+	            case 'N':	// Numeric
+		            $val = sprintf("%s", $val );
+		            break;
+	            case 'L':	// Logical - ? Y y N n T t F f (? when not initialized).
+	                    $val = sprintf("%d", ( ( strtolower($val) === 't' || strtolower($val) === 'y' ) ? 1 : 0) );
+		            break;
+	            case 'T':	// DateTime
+	            case 'D':	// Date
+		            $val = sprintf("'%s'", strftime("%Y-%m-%d %H:%M", strtotime($val) ) );
+		            break;
+            }
             $val = str_replace( "'", "", $val );
-            if($GLOBALS['from_encoding']!="")$val = mb_convert_encoding($val, 'UTF-8', $GLOBALS['from_encoding'] );
-            $a = $a + 1;
+            if($GLOBALS['from_encoding']!="")$val = iconv($GLOBALS['from_encoding'], 'UTF-8', $val);
+			$a = $a + 1;
             if ( $a == 1 ) {
-                $sql1 .="`$key`";
-                $sql2 .="'" . trim( $val ) . "'";
+                $sql3 .="'" . trim( $val ) . "'";
             } else {
-                $sql1 .=",`$key`";
-                $sql2 .=",'$val'";
+                $sql4 .=",'$val'";
             }
         }
-        $sql = "$sql1 $sql2)";
-        if ($conn->query("$sql") === TRUE) {
-			$i++;
-			if ( $i % 100 == 0 ) {
-				echo ".";
-			}
-		} else {
-			echo "Error SQL: ".$conn->error ." >> $sql \n";
-			if ($die_on_mysql_error) {
-				die;
-			}
+		$sql = "$sql3 $sql4),";
+		array_push($row, $sql);		
+		$i++;		
+		if ( $i % 100 == 0 ) {
+			echo ".";
 		}
-  }
 
-	echo "Table $table imported\n";
+  }
+  array_pop($row);  
+  $data=implode("", $row);
+  $sql= "$sql1 $sql2 $data";
+  $sql=rtrim($sql,", ");
+  $sql="$sql; COMMIT;";
+  
+  	fwrite($sqlFile, utf8_encode($sql));
+  	fclose($sqlFile);
+	echo "Table $tbl imported\n";
 
 }
 
